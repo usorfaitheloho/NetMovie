f29524dd12898ce3831ce336ccf1df9b36e6c4dc
usorfaitheloho <usorfaitheloho@gmail.com>
2022-03-16 14:15:27 -0700

    Add Post request

---
+++ src/modules/services/fetchApi.js
@@ -0,0 +1,28 @@
+const get = async (url) => {
+   try {
+    const res = await fetch(url);
+    const data = await res.json();
+    return data
+       
+   } catch (error) {
+     return error  
+   } 
+
+}
+
+const post = async (url,data) => {
+    const config ={
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data)
+    }
+
+    try {
+        const res = await fetch(url, config);
+        const data = await res.json();
+        return data
+           
+       } catch (error) {
+         return error  
+       } 
+}
