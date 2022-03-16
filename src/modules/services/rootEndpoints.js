f413ed730d5058b9f8dc9d738534b271b02ae434
usorfaitheloho <usorfaitheloho@gmail.com>
2022-03-16 11:16:05 -0700

    Add API endpoints

---
+++ src/modules/services/rootEndpoints.js
@@ -0,0 +1,4 @@
+const SHOWS_API = 'https://api.tvmaze.com/shows';
+const INVOLVEMENT_API = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps';
+const LIKES = ':app_id/likes';
+const COMMENTS = ':app_id/comments';
