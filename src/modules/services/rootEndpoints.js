d79b966773656949c0460731bd32c26cfc561abb
unknown <okorojiebube2@gmail.com>
2022-03-18 00:39:42 -0600

    Add comments endpoint

--- src/modules/services/rootEndpoints.js
+++ src/modules/services/rootEndpoints.js
@@ -1,4 +1,5 @@
 export const SHOWS_API = 'https://api.tvmaze.com/shows';
 export const INVOLVEMENT_API = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps';
-export const LIKES = 'IONInnzNi3VRPNlFloAi/likes'
-export const COMMENTS = 'IONInnzNi3VRPNlFloAi/comments'
+export const APP_ID = 'IONInnzNi3VRPNlFloAi'
+export const LIKES = 'likes'
+export const COMMENTS = 'comments'
