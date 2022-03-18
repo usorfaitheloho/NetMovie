8c44b0ec42d0bbdccf707b2064b9050f95956a02
unknown <okorojiebube2@gmail.com>
2022-03-17 21:41:01 -0600

    Add get comments

--- src/modules/services/userServices.js
+++ src/modules/services/userServices.js
@@ -1,16 +1,22 @@
-import { SHOWS_API, INVOLVEMENT_API } from './rootEndpoints'
+import { SHOWS_API, INVOLVEMENT_API, COMMENTS } from './rootEndpoints'
 import { get, post } from './fetchApi'
 import showMovies from '../movies.js'
 import showComments from '../comments'
 
 export const fetchShows = async () => {
-  const data = await get(SHOWS_API)
+	const data = await get(SHOWS_API)
   showMovies(data)
-  showComments(data)
+	showComments(data)
 }
-
 
 export const getUserId = async () => {
-  const id = await post(INVOLVEMENT_API)
-  console.log(id)
+	const id = await post(INVOLVEMENT_API)
+	return id
 }
+
+export const getComments = async () => {
+	const data = await get(`${INVOLVEMENT_API}/${COMMENTS}`)
+
+}
+
+export default getComments
