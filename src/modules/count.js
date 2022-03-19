585957dd498e60da95c8847db36e3ef8d0dcbb2e
unknown <okorojiebube2@gmail.com>
2022-03-19 04:49:48 -0600

    Modify counter function

--- src/modules/count.js
+++ src/modules/count.js
@@ -1,13 +1,13 @@
 export const countShow = list => list.length
 
 const countShows = () => {
-  const showContainer = document.querySelector('main');
+  const showContainer = document.querySelector('.movies');
   const number = showContainer.childElementCount;
   return number;
 }
 
 export const countShowList = () => {
-  const showContainer = document.querySelector('.mainContainer');
+  const showContainer = document.querySelector('.movies');
   const showsList = document.querySelector('.counter');
   const child = showContainer.childElementCount;
   showsList.textContent = `${countShows()}`;
